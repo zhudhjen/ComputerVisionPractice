d0e8ae64aef8757d8a96b7b0c3a21ccdc4d5bcdf
zhudhjen <zhudhjen@gmail.com>
2015-12-22 02:19:51 +0800

    Finish

--- segmentation/main.py
+++ segmentation/main.py
@@ -3,17 +3,27 @@ from skimage.future import graph
 from matplotlib import pyplot as plt
 from mean_shift import mean_shift
 
-image_names = ["sample/campus", "108005/108005", "25098/25098", "78004/78004", "94079/94079"]
+image_names = []
+# image_names += ["176035/176035"]
+# image_names += ["sample/campus", "25098/25098"]
+image_names += ["169012/169012"]
+# image_names += ["108005/108005", "78004/78004", "94079/94079"]
+
+bandwidths = [4]
+radius = [20]
 
 for image_name in image_names:
     print("Processing image:", image_name)
     image = io.imread(image_name + ".jpg")
-    bandwidths = range(20, 101, 20)
     ms_output = {}
     for b in bandwidths:
-        new_image = mean_shift(image, b)
-        ms_output[b] = new_image
-        io.imsave(image_name + "_ms_local_b" + str(b) + ".jpg", new_image)
+        for r in radius:
+            eps = 1
+            new_image = mean_shift(image, radius=r, bandwidth=b, eps=eps)
+            ms_output[b] = new_image
+            io.imsave(image_name + "_ms_local_b" + str(b) +
+                                            "_r" + str(r) +
+                                            "_e" + str(eps) + "_floodfill.jpg", new_image)
 
     print("Computing N-Cut ...")
     label_slic = segmentation.slic(image, compactness=20, n_segments=600)
@@ -21,7 +31,6 @@ for image_name in image_names:
     label_ncut = graph.cut_normalized(label_slic, mean)
     ncut_output = color.label2rgb(label_ncut, image, kind='avg')
     io.imsave(image_name + "_ncut.jpg", ncut_output)
-    print()
 
     # plt.figure().suptitle('Original')
     # io.imshow(image)
--- segmentation/mean_shift.py
+++ segmentation/mean_shift.py
@@ -1,57 +1,33 @@
 from skimage import color
-from scipy.spatial import cKDTree
 from multiprocessing import Pool
 from functools import partial
 import numpy as np
 import sys, time
 
 
-def _kernel_func(r, center, c=50):
-    return np.exp(- np.square(r - center) / c)
+def _kernel_func(r, center, c=5):
+    return np.exp(- np.sum(np.square(r - center), axis=1) / np.square(c) / 2)
 
 
-def _do_mean_shift(pos, image, bandwidth, eps):
-    # print(pos)
-    x_start = max(pos[0] - bandwidth, 0)
-    y_start = max(pos[1] - bandwidth, 0)
-    x_end = min(pos[0] + bandwidth + 1, image.shape[0])
-    y_end = min(pos[1] + bandwidth + 1, image.shape[1])
+def _do_mean_shift(pos, image, radius, bandwidth, eps):
+    x_start = max(pos[0] - radius, 0)
+    y_start = max(pos[1] - radius, 0)
+    x_end = min(pos[0] + radius + 1, image.shape[0])
+    y_end = min(pos[1] + radius + 1, image.shape[1])
     data = np.copy(image[x_start:x_end, y_start:y_end, :]).reshape(-1, 3)
-    return _iterate_mean_shift(image[tuple(pos)], data, eps)
+    return _iterate_mean_shift(image[tuple(pos)], data, bandwidth, eps)
 
 
-def _iterate_mean_shift(color, space, eps):
-    # print(color)
-    weight = _kernel_func(space, color)
+def _iterate_mean_shift(color, space, bandwidth, eps):
+    weight = _kernel_func(space, color, bandwidth)
     new_color = np.average(space, axis=0, weights=weight)
     if np.linalg.norm(new_color - color) > eps:
-        return _iterate_mean_shift(new_color, space, eps)
+        return _iterate_mean_shift(new_color, space, bandwidth, eps)
     else:
         return color
 
 
-def mean_shift(image_rgb, bandwidth=10, eps=1, proc_count=4):
-    start = time.time()
-
-    print("Mean Shift: Bandwidth =", bandwidth, ", EPS =", eps)
-    print("Initializing ...")
-    image = color.rgb2luv(image_rgb)
-    coords = np.rollaxis(np.indices(image.shape[:2]), 0, 3).reshape(-1, 2)
-
-    """
-    print("Constructing KD-Tree ...")
-    sys.setrecursionlimit(10000)
-    kd = cKDTree(pixels, 10)
-    """
-
-    print("Iterating Points ...")
-    partial_iterate = partial(_do_mean_shift, image=image, bandwidth=bandwidth, eps=eps)
-
-    pool = Pool(4)
-    segmentation = np.array(pool.map(partial_iterate, coords))
-
-    """
-    print("Post Processing ...")
+def _color_space_compression(segmentation, bandwidth):
     center_frequency = {}
     for pixel in segmentation:
         pixel_tuple = tuple(pixel.tolist())
@@ -73,10 +49,64 @@ def mean_shift(image_rgb, bandwidth=10, eps=1, proc_count=4):
 
     for index, pixel in enumerate(segmentation):
         segmentation[index] = color_map[tuple(pixel.tolist())]
-    """
+
+
+def _floodfill_compression(image, bandwidth):
+    color_map = np.zeros(image.shape[:2])
+    color_table = {}
+    current_color = 0
+    for pos, tag in np.ndenumerate(color_map):
+        if color_map[pos] == 0:
+            current_color += 1
+            average_color = _do_floodfill(pos, image, bandwidth, current_color, color_map)
+            color_table[current_color] = average_color
+    for pos, tag in np.ndenumerate(color_map):
+        image[pos] = color_table[tag]
+
+
+deltas = np.array([[0, 1], [1, 0], [0, -1], [-1, 0]])
+
+
+def _do_floodfill(pos, image, bandwidth, current_color, color_map):
+    n = 1
+    sum = image[pos].copy()
+    color_map[pos] = current_color
+    points = [pos]
+    while len(points) != 0:
+        current_pos = points.pop(0)
+        for delta in deltas:
+            new_pos = tuple(np.array(current_pos) + delta)
+            if 0 <= new_pos[0] < image.shape[0] and 0 <= new_pos[1] < image.shape[1]:
+                if color_map[new_pos] == 0 and np.linalg.norm(image[new_pos] - image[current_pos]) < bandwidth:
+                    n += 1
+                    color_map[new_pos] = current_color
+                    sum += image[new_pos].copy()
+                    points.append(tuple(list(new_pos)))
+    return sum / n
+
+
+def mean_shift(image_rgb, radius=20, bandwidth=4, eps=1, proc_count=4):
+    start = time.time()
+
+    print("Mean Shift: Radius =", radius, ", Bandwidth =", bandwidth, ", EPS =", eps)
+    print("Initializing ...")
+    image = color.rgb2luv(image_rgb)
+    coords = np.rollaxis(np.indices(image.shape[:2]), 0, 3).reshape(-1, 2)
+
+    print("Iterating Points ...")
+    partial_iterate = partial(_do_mean_shift, image=image, radius=radius, bandwidth=bandwidth, eps=eps)
+
+    pool = Pool(proc_count)
+    segmentation = np.array(pool.map(partial_iterate, coords))
+    pool.close()
+    pool.join()
+
+    print("Post Processing ...")
+    sys.setrecursionlimit(image.shape[0] * image.shape[1] + 100)
+    segmentation = segmentation.reshape(image.shape)
+    _floodfill_compression(segmentation, bandwidth)
 
     print("Ending ...")
-    segmentation = segmentation.reshape(image.shape)
     segmentation = color.luv2rgb(segmentation)
 
     end = time.time()
